c25b7a44b570c0b8f4ef0ce763076fab1ba5400c
Lenzo, Colin James <cjlenzo@iu.edu>
2024-04-05 20:37:35 -0400

    Group Work 9 Part 6

--- src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/controllers/MergerController.java
+++ src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/controllers/MergerController.java
@@ -1,13 +1,17 @@
 package edu.iu.habahram.DinerPancakeHouseMerge.controllers;
 
+import edu.iu.habahram.DinerPancakeHouseMerge.model.Menu;
 import edu.iu.habahram.DinerPancakeHouseMerge.model.MenuItem;
+import edu.iu.habahram.DinerPancakeHouseMerge.repository.CafeRepository;
 import edu.iu.habahram.DinerPancakeHouseMerge.repository.DinerRepository;
+import edu.iu.habahram.DinerPancakeHouseMerge.repository.MergerRepository;
 import edu.iu.habahram.DinerPancakeHouseMerge.repository.PancakeHouseRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -19,24 +23,37 @@ public class MergerController {
 
     DinerRepository dinerRepository;
     PancakeHouseRepository pancakeHouseRepository;
+    MergerRepository mergerRepository;
+    CafeRepository cafeRepository;
 
-    public MergerController(DinerRepository dinerRepository, PancakeHouseRepository pancakeHouseRepository) {
+    public MergerController(DinerRepository dinerRepository, PancakeHouseRepository pancakeHouseRepository, CafeRepository cafeRepository, MergerRepository mergerRepository) {
         this.dinerRepository = dinerRepository;
         this.pancakeHouseRepository = pancakeHouseRepository;
+        this.cafeRepository = cafeRepository;
+        this.mergerRepository = mergerRepository;
     }
 
     @GetMapping
     public List<MenuItem> get() {
-        List<MenuItem> menuItems = new ArrayList<>();
+        ArrayList<MenuItem> items = new ArrayList<>();
+        ArrayList<Menu> menuItems = mergerRepository.getTheMenus();
         Iterator<MenuItem> lunchItems = dinerRepository.getTheMenuIterator();
         while(lunchItems.hasNext()) {
-            menuItems.add(lunchItems.next());
+            items.add(lunchItems.next());
         }
 
         Iterator<MenuItem> breakfastItems = pancakeHouseRepository.getTheMenuIterator();
         while(breakfastItems.hasNext()) {
-            menuItems.add(breakfastItems.next());
+            items.add(breakfastItems.next());
         }
-        return menuItems;
+
+        Iterator<MenuItem> cafeItems = cafeRepository.getTheMenuIterator();
+        while(cafeItems.hasNext()) {
+            items.add(cafeItems.next());
+        }
+
+        items.sort((item1, item2) -> item1.getName().compareTo(item2.getName()));
+
+        return items;
     }
 }
--- src/test/java/edu/iu/habahram/DinerPancakeHouseMerge/controllers/MergerControllerTest.java
+++ src/test/java/edu/iu/habahram/DinerPancakeHouseMerge/controllers/MergerControllerTest.java
@@ -1,7 +1,9 @@
 package edu.iu.habahram.DinerPancakeHouseMerge.controllers;
 
 import edu.iu.habahram.DinerPancakeHouseMerge.model.MenuItem;
+import edu.iu.habahram.DinerPancakeHouseMerge.repository.CafeRepository;
 import edu.iu.habahram.DinerPancakeHouseMerge.repository.DinerRepository;
+import edu.iu.habahram.DinerPancakeHouseMerge.repository.MergerRepository;
 import edu.iu.habahram.DinerPancakeHouseMerge.repository.PancakeHouseRepository;
 import org.junit.jupiter.api.Test;
 
@@ -15,8 +17,10 @@ class MergerControllerTest {
     public void testGetAll() {
         DinerRepository dinerRepository = new DinerRepository();
         PancakeHouseRepository pancakeHouseRepository = new PancakeHouseRepository();
-        MergerController mergerController = new MergerController(dinerRepository, pancakeHouseRepository);
-        List<MenuItem> allMenuItems = mergerController.getAll();
+        CafeRepository cafeRepository = new CafeRepository();
+        MergerRepository mergerRepository = new MergerRepository();
+        MergerController mergerController = new MergerController(dinerRepository, pancakeHouseRepository, cafeRepository, mergerRepository);
+        List<MenuItem> allMenuItems = mergerController.get();
         for (MenuItem item : allMenuItems) {
             System.out.println(item);
         }
